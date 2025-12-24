aed94e88de0ec7e77420f820314a995cf852ead0
jaimeaznar <jaimeaznarupv@gmail.com>
2025-12-24 16:24:32 +0100

    Add Railway deployment configuration

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,20 @@
 # wsgi.py
 import os
+import sys
 from dotenv import load_dotenv
 load_dotenv()
 
+print(f"Starting wsgi.py, PORT={os.getenv('PORT', 'NOT SET')}", flush=True)
+sys.stdout.flush()
+
 from app import create_app
 
+print("Importing create_app done", flush=True)
+
 app = create_app()
+
+print("App created, ready for gunicorn", flush=True)
+sys.stdout.flush()
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
